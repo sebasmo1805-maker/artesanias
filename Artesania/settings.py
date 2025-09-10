bd6312a3fcdeaa5aa76e11a599ec01dc748c3cfd
sebasmo1805-maker <sebasmo1805@gmail.com>
2025-09-10 15:39:51 -0500

    Agrego whitenoise para manejar archivos estáticos 2

--- Artesania/settings.py
+++ Artesania/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'main.CustomUser'
